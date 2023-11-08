6129d4aa15d5acf93fc18484fec305085c6ff4b3
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2023-11-08 16:22:16 +0100

    Display example name and html link

--- docs/source/_ext/ceinclude.py
+++ docs/source/_ext/ceinclude.py
@@ -3,13 +3,17 @@ import json
 
 from docutils import nodes
 from sphinx.directives.code import LiteralInclude, container_wrapper
+from os import path
 
 class CeIncludeDirective(LiteralInclude):
     """ LiteralInclude with a Compiler Explorer link"""
 
     def run(self):
         retnode = super().run()[0]
+
         _, filename = self.env.relfn2path(self.arguments[0])
+        retnode = container_wrapper(self, retnode, path.basename(filename))
+
         source = open(filename, "r").read()
         client_state = {
             "sessions": [
@@ -27,10 +31,14 @@ class CeIncludeDirective(LiteralInclude):
                 }
             ]
         }
+
         encoded = base64.urlsafe_b64encode(json.dumps(client_state).encode())
-        retnode = container_wrapper(self, retnode, "https://godbolt.org/clientstate/" + encoded.decode())
-        # breakpoint()
+        ce_link = "https://godbolt.org/clientstate/" + encoded.decode()
+        doc_link = f'<a href="{ce_link}" target="_blank">Edit on Compiler Explorer</a>'
+
+        retnode += nodes.raw(rawsource = doc_link, text = doc_link, format="html")
         return [retnode]
+
 
 def setup(app):
     app.add_directive("ceinclude", CeIncludeDirective)
