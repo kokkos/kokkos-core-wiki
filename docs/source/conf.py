1c2a53e3bbbcfcaa295d529e7a0452d2ff557a4c
JBludau <jakob.bludau@tum.de>
2024-11-18 09:48:34 -0500

    removed dependency on linuxdoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,7 @@ extensions = ["myst_parser",
               "sphinx.ext.intersphinx",
               "sphinx_copybutton",
               "sphinx_design",
-              "cppkokkos",
-              "linuxdoc.rstFlatTable"]
+              "cppkokkos"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
